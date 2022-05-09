5945e1440b877392eaa62f3c052018be0e94f680
Dazikiri Anikar <mierzwinski.b@gmail.com>
2022-05-09 19:56:14 +0200

    Added exemplary data to be inserted into the database upon the execution of the program.

--- src/main/java/bm/app/mongodbexample/MongoDbConfig.java
+++ src/main/java/bm/app/mongodbexample/MongoDbConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
  * @Component means the class will be managed by Spring Context.
  */
@@ -26,7 +28,18 @@ public class MongoDbConfig {
      */
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
-        
+        Article article = new Article();
+        article.setAuthor("Vargothan Ganlinzarak");
+        article.setTitle("Forge with your heart, not hands.");
+
+        Comment commentOne = new Comment();
+        commentOne.setText("Good lad.");
+        Comment commentTwo = new Comment();
+        commentTwo.setText("I could hardly stay calm reading this wisdom.");
+
+        article.setCommentList(Arrays.asList(commentOne, commentTwo));
+
+        mongoTemplate.insert(article);
     }
 
 }
